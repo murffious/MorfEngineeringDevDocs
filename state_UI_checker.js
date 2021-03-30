7fa44f659ce5883dc1b566b69339a5cd9c1015ad
Paul Murff <25558342+murffious@users.noreply.github.com>
2021-03-30 14:53:38 -0600

    have I already done this - find it

---
+++ state_UI_checker.js
@@ -0,0 +1,18 @@
+/* 
+
+  React usees state but this could be for Angular or anything using this pattern. Even Vue.
+
+  I have noticed while jumping in and seeing errors on UI issues in front end apps that all these quesitons need to be addressed for each part of the UI 
+  What happens to state when these things happen OR what type of data[state] persistence do you expect to have - testing checklist sheet for these items
+   newer developers or some veterans who don't ansser all the UIUX quesitons of what to do in each case need to do this more
+  1. Refresh
+  2. Hit back on the browser
+  3. Hit back on bread crumbs or step of a process and forward to (back and forth)
+  4. log in and or log out processes
+  variation /random combination of all of these-- 
+  TODO - use facebook as example of how this sohould be and compare to amazon or twitter 
+
+  Things like spinner, UI dummy loader components - I have wanted to write this down and now realize I may have another docs for this ...
+
+*/
+
